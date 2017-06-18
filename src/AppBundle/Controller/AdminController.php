1c0113022575daf76af97603dd2ca9624aa233eb
Andy D. Navarro Taño <andaniel05@gmail.com>
2017-06-18 16:55:43 -0400

    Temp

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,22 +2,77 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 
 class AdminController extends BaseAdminController
 {
-    public function createNewUserEntity()
+    protected function createNewUserEntity()
     {
         return $this->get('fos_user.user_manager')->createUser();
     }
 
-    public function prePersistUserEntity($user)
+    protected function prePersistUserEntity($user)
     {
         $this->get('fos_user.user_manager')->updateUser($user, false);
     }
 
-    public function preUpdateUserEntity($user)
+    protected function preUpdateUserEntity($user)
     {
         $this->get('fos_user.user_manager')->updateUser($user, false);
+    }
+
+    protected function createArticleListQueryBuilder()
+    {
+        $checker = $this->get('security.authorization_checker');
+        $repository = $this->getDoctrine()
+            ->getRepository('AppBundle:Article');
+
+        if ($checker->isGranted('ROLE_ADMIN')) {
+            $query = $repository->createQueryBuilder('a')->getQuery();
+        } else {
+            $user = $this->getUser();
+            $query = $repository->createQueryBuilder('a')
+                ->where('a.author = :author_id')
+                ->setParameter('author_id', $user->getId())
+                ->getQuery();
+        }
+
+        return $query;
+    }
+
+    public function listUserAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return parent::listAction();
+    }
+
+    public function newUserAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return parent::newAction();
+    }
+
+    public function editUserAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return parent::editAction();
+    }
+
+    public function showUserAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return parent::showAction();
+    }
+
+    public function deleteUserAction()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        return parent::deleteAction();
     }
 }
